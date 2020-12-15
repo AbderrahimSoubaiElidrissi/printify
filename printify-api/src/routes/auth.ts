0cc4cb8bb41d7f7fe774c40babc965a312d41ad3
Abderrhim Soubai <gx.coder@gmail.com>
2020-12-15 01:15:40 +0100

    fix(core): merge me-route

--- printify-api/src/routes/auth.ts
+++ printify-api/src/routes/auth.ts
@@ -3,12 +3,8 @@ import AuthController from "../controllers/auth";
 import { checkJwt } from "../middlewares/checkJwt";
 
 const router = Router();
-<<<<<<< HEAD
-// Login route
-=======
 router.get("/me", [checkJwt], AuthController.me);
 
->>>>>>> feature/me-route
 router.post("/login", AuthController.login);
 
 router.post("/register", AuthController.register);
